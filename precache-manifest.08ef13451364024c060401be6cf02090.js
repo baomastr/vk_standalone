344c5fe2bd3707be13cf29752d5011a6aeed8e96
d.lisyanskiy <d.lisyanskiy@tinkoff.ru>
2020-06-19 18:53:57 +0300

    Updates

---
+++ precache-manifest.08ef13451364024c060401be6cf02090.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96a7d4fa530b40e56740723917dbb18",
+    "url": "/vk_standalone/index.html"
+  },
+  {
+    "revision": "b4eb8e7f77ddd795f876",
+    "url": "/vk_standalone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6394c8525c2b2a8e51bb",
+    "url": "/vk_standalone/static/js/2.2a160814.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk_standalone/static/js/2.2a160814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4eb8e7f77ddd795f876",
+    "url": "/vk_standalone/static/js/main.22e92d87.chunk.js"
+  },
+  {
+    "revision": "69de1021f7de7013bab9",
+    "url": "/vk_standalone/static/js/runtime-main.87aad493.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vk_standalone/static/media/logo.5d5d9eef.svg"
+  }
+]);
