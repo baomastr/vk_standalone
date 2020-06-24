e29ac2128626a81997a59a2e9394b8a6de8ed16e
d.lisyanskiy <d.lisyanskiy@tinkoff.ru>
2020-06-24 19:15:09 +0300

    add posts search

--- src/App.js
+++ src/App.js
@@ -5,50 +5,74 @@ import {VK} from 'vk-call';
 import './App.css';
 
 let vkApi = new VK({
-  token: '207dfb85136fe9e029cb08676b43eecbc2a115dfd289acf955d3c606a9bf825ea1a4f0bc80b5bb00dc9fd',
-  version: "5.50",
-  timeout: 10000
+	token: '207dfb85136fe9e029cb08676b43eecbc2a115dfd289acf955d3c606a9bf825ea1a4f0bc80b5bb00dc9fd',
+	version: "5.50",
+	timeout: 10000
 });
 
 function App() {
-  const [postLink, setPostLink] = useState('');
-  const [commentText, setCommentText] = useState('');
-  const [commentParams, setCommentParams] = useState({});
+	const [searchString, setSearchString] = useState('');
+	const [postLink, setPostLink] = useState('');
+	const [commentText, setCommentText] = useState('');
+	const [commentParams, setCommentParams] = useState({});
+	const [searchResult, setSearchResult] = useState({});
 
-  const createComment = () => {
-    if (!vkApi) {
-      return
-    }
-    vkApi.call("wall.createComment", { ...commentParams, message: commentText })
-      .then(comment_id => {
-        setCommentText('');
-        setPostLink('');
-        console.log(comment_id);
-      }
-    );
-  };
+	const searchPosts = () => {
+		vkApi.call('newsfeed.search', {q: searchString, extended: 1}).then(res => {
+			setSearchResult(res);
+			console.log(res)
+		})
+	};
 
-  useEffect(()=>{
-    if (postLink) {
-      try {
-        const {query: {w}} = url.parse(postLink, true);
-        const [owner_id, post_id] = w.match(/\d+/g);
-        setCommentParams({owner_id, post_id});
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  }, [postLink]);
+	const createComment = () => {
+		if (!vkApi) {
+			return
+		}
+		vkApi.call("wall.createComment", {...commentParams, message: commentText})
+			.then(res => {
+					setCommentText('');
+					setPostLink('');
+					console.log(res);
+				}
+			);
+	};
 
-  return (
-    <div className="App">
-      <input type="text" value={postLink} placeholder="ссылка на пост" onChange={({currentTarget: {value}}) => setPostLink(value)}/>
-      <br/>
-      <input type="text" value={commentText} placeholder="комментарий" onChange={({currentTarget: {value}}) => setCommentText(value)}/>
-      <br/><br/><br/>
-      <button onClick={createComment}>send comment</button>
-    </div>
-  );
+	useEffect(() => {
+		if (postLink) {
+			try {
+				const {query: {w}} = url.parse(postLink, true);
+				const [owner_id, post_id] = w.match(/\d+/g);
+				setCommentParams({owner_id, post_id});
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	}, [postLink]);
+
+	return (
+		<div className="App">
+			<input type="text" value={searchString} placeholder="искать посты"
+						 onChange={({currentTarget: {value}}) => setSearchString(value)}/>
+			<br/>
+			<br/>
+			<button onClick={searchPosts}>search posts</button>
+			<br/>
+			<br/>
+			<input type="text" value={postLink} placeholder="ссылка на пост"
+						 onChange={({currentTarget: {value}}) => setPostLink(value)}/>
+			<br/>
+			<br/>
+			<input type="text" value={commentText} placeholder="комментарий"
+						 onChange={({currentTarget: {value}}) => setCommentText(value)}/>
+			<br/><br/>
+			<button onClick={createComment}>send comment</button>
+			<br/>
+			<br/>
+			{searchResult && <pre>
+        {JSON.stringify(searchResult.items, null, 2)}
+      </pre>}
+		</div>
+	);
 }
 
 export default App;
